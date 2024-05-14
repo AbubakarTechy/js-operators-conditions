5cab5fa6e3eb4d56493d5951e0e1dc9cbc565b2f
Abubakar Rasheed <131538521+abubakarrasheed@users.noreply.github.com>
2024-05-15 00:37:46 +0500

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,36 @@ if(num%2===0){
 }else{
     console.log("Number is odd");
 }
+
+//_____TERNARY OPERATOR
+
+let agee=25;
+let result= agee>18 ? "Adult" : "Not adult";
+console.log(result);
+
+//______Q==1 check num is mul of 5 or not
+
+let numb=prompt("Enter a number:");
+if(numb%5==0){
+    console.log("Number is mul of 5");
+    
+} else {
+    console.log("Number is not mul of 5");
+}
+
+
+//______Q==2 GRADES CHECK
+
+
+let score=prompt("Enter Marks:");
+if(score>=80 && score<=100){
+    console.log("Grade A");
+} else if (score>=70 && score<89){
+    console.log("Grade B");
+} else if(score>=60 && score<69){
+        console.log("Grade C");
+}else if(score>=50 && score<59){
+            console.log("Grade D");
+} else {
+              console.log("Grade F");
+}
