b2d80a7e41058b312f656a25fd3b4a84e9552746
Abubakar Rasheed <131538521+abubakarrasheed@users.noreply.github.com>
2024-05-14 11:54:34 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+_____________________//Conditional Statements
+
+let age=25;
+if(age>18){
+    console.log("He can Vote");
+} else{
+    console.log("He cannot vote");
+}
+
+//ODD --> EVEN code
+
+let num=10;
+if(num%2===0){
+    console.log("Number is even");
+}else{
+    console.log("Number is odd");
+}
